428063609761ef7adaa16323e7a4614e6796fd4a
qwerttuuii <talk2teespicy@gmail.com>
2026-03-21 22:32:10 +0100

    locoloco

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,33 +1,50 @@
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [pathname]);
 
   return (
     <nav className="navbar">
       <div className="logo">FID ASIKA ILOBI & CO</div>
 
-  <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-  {menuOpen ? "✖" : "☰"}
-</div>
+      <button
+        type="button"
+        className="menu-icon"
+        aria-label={menuOpen ? "Close menu" : "Open menu"}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? "\u2715" : "\u2630"}
+      </button>
 
       <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li>
-          <NavLink to="/" end>Home</NavLink>
+          <NavLink to="/" end onClick={() => setMenuOpen(false)}>Home</NavLink>
         </li>
         <li>
-          <NavLink to="/about">About</NavLink>
-        </li>
-       
-        <li>
-          <NavLink to="/OurTeam">OurTeam</NavLink>
+          <NavLink to="/about" onClick={() => setMenuOpen(false)}>About</NavLink>
         </li>
         <li>
-          <NavLink to="/contact">Contact us</NavLink>
+          <NavLink to="/OurTeam" onClick={() => setMenuOpen(false)}>OurTeam</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" onClick={() => setMenuOpen(false)}>Contact us</NavLink>
         </li>
       </ul>
+
+      {menuOpen && (
+        <div
+          className="menu-backdrop show"
+          onClick={() => setMenuOpen(false)}
+          aria-hidden="true"
+        />
+      )}
     </nav>
   );
 }
